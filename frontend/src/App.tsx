3242447d03746dd3425a46b7cab27b873759fea2
Cristiano Xavier <cristianoxc@yahoo.com.br>
2021-05-05 22:47:51 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+      </div>
+      <DataTable/>
+      <Footer/>
+    </>
   );
 }
 
 export default App;
+
